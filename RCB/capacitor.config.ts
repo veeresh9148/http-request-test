d2a46c00ec1f13503b9889bad7344d4146c6b777
Veeresh K <veeresh91488@gmail.com>
2023-01-16 17:10:01 +0530

    vk

---
+++ RCB/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RCB',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
